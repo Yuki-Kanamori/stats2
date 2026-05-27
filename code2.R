a1438f17cc96ebc5fc1557a335aa041a216df1c7
Yuki-Kanamori <y.kanamory@gmail.com>
2026-05-27 12:25:51 +0900

    revised

--- code2.R
+++ code2.R
@@ -30,10 +30,18 @@ mean(seed$x)
 
 
 # 種子数と体長の関係を散布図で表す -------------------------------------------------------------------------
+g = ggplot(data = seed, aes(x = x, y = y))
+p = geom_point()
+labs = labs(x = "Body size", y = "Seed")
+g+p+labs+theme_bw()
 
 
 
 # 肥料の有無に分けて，箱ひげ図を作成する ------------------------------------------------------
+g = ggplot(data = seed, aes(x = f, y = y))
+b = geom_boxplot()
+labs = labs(x = "肥料", y = "Seed")
+g+b+labs+theme_gray(base_family = "HiraKakuPro-W3")
 
 
 
@@ -47,7 +55,12 @@ logLik(fit1)
 
 
 # モデル2を推定する -------------------------------------------------------------
-
+fit2 = glm(y ~ f, data = seed, family = poisson(link = "log"))
+summary(fit2)
+logLik(fit2)
 
 
 # モデル3を推定する ------------------------------------------------------
+fit3 = glm(y ~ x + f, data = seed, family = poisson(link = "log"))
+summary(fit3)
+logLik(fit3)
